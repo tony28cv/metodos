46571809c20b1f3b591b292906a82a1bbabfbeab
tony28cv <43261362+tony28cv@users.noreply.github.com>
2018-09-26 20:12:38 -0500

    Add files via upload

---
+++ grafica2.m
@@ -0,0 +1,13 @@
+prompt=('Introduce el numero de iteraciones');
+n=input(prompt);
+prompt=('Intrtoduce el valor inicial:');
+a=input(prompt);
+VectorIt=1:1:n
+for i=1:1:n;
+        if i==1
+            x(i)=-cos(a)/3
+        else
+            x(i)=-cos(x(i-1))/3
+        end
+end
+plot(VectorIt,x);
