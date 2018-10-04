1620271a178b3435530eb7baba594e78674ebcbc
tony28cv <43261362+tony28cv@users.noreply.github.com>
2018-10-03 19:41:09 -0500

    Add files via upload

---
+++ agua.m
@@ -0,0 +1,13 @@
+propmt=('litros consumidos')
+n=input(propmt)
+if n<=50
+    cuota=150
+    if n>50
+        if n<200
+            cuota=((150)+(n50)*3)
+        end
+    end
+end
+if n>200
+    cuota=(600)+(n-200)*5
+end
